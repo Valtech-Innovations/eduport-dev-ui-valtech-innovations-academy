9ea9082d041d85be51fc9734a27f178c79c7b8e5
Vivek Bhawsar <93441216+VivekBhawsar18@users.noreply.github.com>
2023-08-24 20:01:24 +0530

    under dev home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignupPage from './container/Signup';
-
+import UnderDev from './component/underDev';
 
 function App() {
 
@@ -10,6 +10,7 @@ function App() {
 
       <Router>
         <Routes>
+          <Route path="/" element={<UnderDev />} />
           <Route path='/sign-up' element={<SignupPage />} />
         </Routes>
       </Router>
