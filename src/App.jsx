06df9eb0724b1540400c4dc6cbf810c580236063
Mujjamil07 <138234115+Mujjamil07@users.noreply.github.com>
2023-08-27 13:09:48 +0530

    Code Updated
    Course Details

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './container/Signup';
 import Signin from './container/Signin';
 import UnderDev from './component/underDev';
+import Course from './Course';
 
 function App() {
 
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" element={<UnderDev />} />
           <Route path='/sign-up' element={<Signup />} />
           <Route path='/sign-in' element={<Signin />} />
+          <Route path='/Course' element={<Course />} />
+       
         </Routes>
       </Router>
     </>
