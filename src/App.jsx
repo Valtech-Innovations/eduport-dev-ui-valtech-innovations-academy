30670e03459ef20ecb3abff61b1dc0ce06cb34bc
Sangeeta Shrivas <shrivassangeeta57@gmail.com>
2023-08-29 13:47:36 +0530

    Action page
    action perform page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './container/Signup';
 import Signin from './container/Signin';
 import Header from './component/layout/Header'
+import Banner from './component/layout/Banner'
+import Action from './component/layout/Action'
 import UnderDev from './component/underDev';
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
           <Route path='/sign-up' element={<Signup />} />
           <Route path='/sign-in' element={<Signin />} />
           <Route path='/header' element={<Header />} />
+          <Route path='/banner' element={<Banner />} />
+          <Route path='/action' element={<Action />} />
         </Routes>
       </Router>
     </>
