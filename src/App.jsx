d7b7b1067da5d6161e5b7bd26ca60476f7f0c9b6
Nazim khan <95021702+nazim2541@users.noreply.github.com>
2023-09-16 14:46:41 +0530

    update home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './component/layout/Header'
 import Footer from './component/layout/Footer'
 import Signup from './container/Signup';
-import Signin from './container/Signin';
+
 import Courses from './container/Courses';
 import Action from './container/Action';
 import Banner from './container/Banner';
 import Decoration from './container/Decoration';
 import Feedback from './container/Feedback';
 import Instructor from './container/Instructor';
-import UnderDev from './component/underDev';
+
 import Layout from './component/layout/Layout';
 import Home from './pages/Home';
 // import Signin from './container/Signin';
 import UnderDev from './component/underDev';
-import ForgetPassword from './container/ForgetPassword';
 import Signin from './container/Signin';
 
 function App() {
@@ -37,7 +36,7 @@ function App() {
           <Route path='/courses' element={<Courses />} />
           <Route path='/decoration' element={<Decoration />} />
           <Route path='/Instructor' element={<Instructor />} />
-          <Route path='/forget-password' element={<ForgetPassword />} />
+          {/* <Route path='/forget-password' element={<ForgetPassword />} /> */}
         </Routes>
       </Router>
     </>
